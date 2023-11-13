cd2197c3bac49600daabb407db5d1c423a525737
A K <akahly@gmail.com>
2023-11-13 11:31:15 -0500

    Psuedo Coding

---
+++ tracker.js
@@ -0,0 +1,13 @@
+// Install required dependants
+
+// created a connection to SQL
+
+// error throw 
+
+// put choices for inquirer
+
+// add functions 
+
+// insert user input
+
+// user selection 
