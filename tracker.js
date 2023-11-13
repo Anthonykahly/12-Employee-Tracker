b345f02e11cd00040337bf44720b064c47ed8190
A K <akahly@gmail.com>
2023-11-13 11:45:25 -0500

    starting on js

--- tracker.js
+++ tracker.js
@@ -1,13 +1,23 @@
-// Install required dependants
+// Installed/Required Dependencies
+const cTable = require("console.table");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
-// created a connection to SQL
-
-// error throw 
+// Create connection to SQL DB
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    // Change this password to match the password in your DB (Yours may not be root)
+    password: "root",
+    database: "employeetracker"
+});
+// error throw
 
 // put choices for inquirer
 
-// add functions 
+// add functions
 
 // insert user input
 
-// user selection 
+// user selection
